5f3aeaf1c5005c92be2121304d6b5815d2a9d404
balibou <benjamin.cherion@gmail.com>
2015-07-25 11:26:24 +0200

    Possibility to open a chatroom

--- client/views/chat.js
+++ client/views/chat.js
@@ -3,3 +3,19 @@ Template.chat.helpers({
     return Meteor.users.find({ "status.online": true , _id: {$ne: Meteor.userId()} });
   }
 });
+
+Template.chat.events({
+  'click .user':function(){
+
+    var chatroom ={
+      userId: this._id
+    };
+
+    Meteor.call('chatroomsCheck', chatroom, function(error, result) {
+      if (error){
+        return alert(error.reason);
+      }
+      Router.go('chatroom', {_id: result._id});
+    });
+  }
+});

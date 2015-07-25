2c24ff403ad24e5a8c91e1fa9bfd65dfa7a104d7
balibou <benjamin.cherion@gmail.com>
2015-07-25 14:45:16 +0200

    user can send a message in chatroom

--- server/publications.js
+++ server/publications.js
@@ -4,3 +4,7 @@ Meteor.publish("onlusers",function(){
 Meteor.publish("chatrooms",function(){
   return ChatRooms.find({});
 });
+
+Meteor.publish('messages', function(){
+  return Messages.find({});
+});
